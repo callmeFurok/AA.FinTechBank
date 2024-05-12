575aceb54dbbb94eb0a2e2394c4c9e98256b2eb6
Alfredo Aguirre <thefurok@gmail.com>
2024-05-11 20:57:15 -0500

    Add db connection string

--- AA.FinTechBank.Api/Program.cs
+++ AA.FinTechBank.Api/Program.cs
@@ -1,3 +1,6 @@
+using AA.FinTechBank.Infrastructure.DbContextApp;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// Add Connection DB
+builder.Services.AddDbContext<ApplicationDbContext>(options=>
+options.UseNpgsql(builder.Configuration.GetConnectionString("PostresDb")));
 
 var app = builder.Build();
 
