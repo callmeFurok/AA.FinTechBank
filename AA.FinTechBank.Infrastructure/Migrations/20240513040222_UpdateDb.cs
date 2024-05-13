420d912ad9ef1ed253fc4d752f16c0e670329e00
Alfredo Aguirre <thefurok@gmail.com>
2024-05-13 00:07:43 -0500

    Add all repository methods and make a new migration

---
+++ AA.FinTechBank.Infrastructure/Migrations/20240513040222_UpdateDb.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AA.FinTechBank.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ClientType",
+                table: "Clients",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClientMaritalStatus",
+                table: "Clients",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ClientType",
+                table: "Clients",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ClientMaritalStatus",
+                table: "Clients",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
